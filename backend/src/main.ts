1d2d2e329c1a6b053496c034bae75aa69e18a84b
diogodsg <diogodasilvagouveia@gmail.com>
2025-10-27 21:24:51 -0300

    :hammer: update cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://app.forji.me'],
     credentials: true,
   });
 
